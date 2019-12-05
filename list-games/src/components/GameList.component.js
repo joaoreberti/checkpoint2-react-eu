e02d5090d877faade0c69a9a26c55114db02a1a9
Maria <fmariamarinescu@gmail.com>
2019-12-05 10:26:00 +0200

    installes axios and imports it

--- list-games/src/components/GameList.component.js
+++ list-games/src/components/GameList.component.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 
 
 class GameList extends React.Component {
 constructor(props) {
     super(props);
+    this.state = {
+
+    }
 }
+
+retrieveGameList = () => {
+    
+}
+
  
 }
 
