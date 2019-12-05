b9b0e35487dcac770d74d7778193a57671e09e24
Maria <fmariamarinescu@gmail.com>
2019-12-05 10:41:22 +0200

    implemets fetch method and set the state with the full array of games

--- list-games/src/components/GameList.component.js
+++ list-games/src/components/GameList.component.js
@@ -6,15 +6,30 @@ class GameList extends React.Component {
 constructor(props) {
     super(props);
     this.state = {
-
+        games: []
     }
 }
 
 retrieveGameList = () => {
-    
+    axios.get('https://wild-games.herokuapp.com/api/v1')
+    .then(response => {
+        this.setState({
+            games: response.data
+        })
+    })
+  
+}
+componentWillMount() {
+    this.retrieveGameList();
 }
 
- 
+render() {
+    return(
+        <div>
+            <button onClick={this.showMe}>Click</button>
+        </div>
+    )
+}
 }
 
 
