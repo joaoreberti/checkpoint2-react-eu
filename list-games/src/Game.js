bcf81a322b262c63b1fa0b1aa26d26760408e288
Nicolas Huens <huensnico@gmail.com>
2019-12-09 10:07:40 +0100

    create Gamelist and Game components

---
+++ list-games/src/Game.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Game extends React.components {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    } 
+}
+
+export default Game;
