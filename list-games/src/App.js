5cdcfefba128b070525a87391281b2b1e2950854
leti <meteorite@gmail.com>
2019-12-09 12:39:52 +0100

    game

---
+++ list-games/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import './GameList.css';
+import './Game.css';
+
+function App() {
+  return (
+
+    <div className="game" >
+      <GameList />
+      <Game />
+    
+    </div>
+
+
+  );
+}
+
+export default App;
---
+++ list-games/src/GameList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import axios from 'axios';
+import './GameList.css';
+import Game from './Game';
+
+function GameList() {
+  return (
+
+    List Games
+    state = {
+      GameList = ["Luc", "Mass", ]
+    }
+    
+    render()
+
+
+
+  );
+}
+
+export default GameList;
