1400237af616cd0fde29c9c95d2436d44457cc87
Nicolas Huens <huensnico@gmail.com>
2019-12-09 12:54:13 +0100

    improving game.js and gamelist.js

--- list-games/src/Game.js
+++ list-games/src/Game.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Game extends React.components {
+class Game extends Component {
     constructor(props) {
         super(props);
         this.state = {
 
         }
     } 
+
+    render() {
+        return(
+            <button onClick={this.closeTheGame}>Close</button>
+        )
+    }
+
 }
 
 export default Game;
--- list-games/src/Gamelist.js
+++ list-games/src/Gamelist.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Gamelist extends React.component {
+const axios = require('axios');
+
+class Gamelist extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            games: [],
         }
     }
+
+    componentDidMount() {
+        const list = async () => {
+            const response = await axios.get('https://wild-games.herokuapp.com/api/v1')
+            console.log(response)
+            }
+    }
+          
+    
+
+    render() {
+        const { games } = this.state;
+        return (
+            <ul>
+                {games.map((game, index) => {
+                    return(
+                        <div>
+                          hello!
+                        </div>
+                    )
+                }
+                )}
+            </ul>
+        )
+    }
+
 }
 
 export default Gamelist;
