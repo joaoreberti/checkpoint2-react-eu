5ba2f3b38915e8f7db9ab6002d53e2ed6e69f8e0
rebertionfire <joaoreberti@gmail.com>
2020-03-26 20:41:39 +0000

    map and filter added

--- list-games/src/components/Game.js
+++ list-games/src/components/Game.js
@@ -5,28 +5,38 @@ class Game extends Component{
     constructor(props){
         super(props)
         this.state = {
-            image: this.props.gameData.background_image,
-            color: this.props.gameData.dominant_color,
-            id: this.props.gameData.id,
-            name: this.props.gameData.name,
-            rating: this.props.gameData.rating,
-            released: this.props.gameData.released,
-            saturated_color: this.props.gameData.saturated_color,
-            slug:this.props.gameData.slug,
-            clip: this.props.gameData.clip.clip,
-            screenshots: this.props.gameData.short_screenshots
 
+            gameData:{
+                background_image: this.props.gameData.background_image,
+                dominant_color: this.props.gameData.dominant_color,
+                id: this.props.gameData.id,
+                name: this.props.gameData.name,
+                rating: this.props.gameData.rating,
+                released: this.props.gameData.released,
+                saturated_color: this.props.gameData.saturated_color,
+                slug:this.props.gameData.slug,
+                clip: this.props.gameData.clip,
+                short_screenshots: this.props.gameData.short_screenshots
+            }
+            
+
+        }
+    }
+
+    componentDidUpdate(prevProps){
+        if(prevProps !== this.props){
+            this.setState(this.props)
         }
     }
     render(){
         return(
             <div className="cell">
-                <h2>{this.state.name}</h2>
-                <img src ={`${this.state.image}`} />
-                <video src={`${this.state.clip}`} controls ></video>
-        <p>Rating: {this.state.rating}</p>
-        <p>Released : {this.state.released}</p>
-        <button onClick={() => this.props.toRemove(this.state.id)}>Remove</button>
+                <h2>{this.state.gameData.name}</h2>
+                <img src ={`${this.state.gameData.background_image}`} />
+                <video src={`${this.state.gameData.clip.clip}`} controls ></video>
+        <p>Rating: {this.state.gameData.rating}</p>
+        <p>Released : {this.state.gameData.released}</p>
+        <button onClick={() => this.props.toRemove(this.props.number)}>Remove</button>
 
             </div>
         )
--- list-games/src/components/Gamelist.js
+++ list-games/src/components/Gamelist.js
@@ -1,37 +1,64 @@
-import React, {Component} from "react"
-import Game from "./Game"
-import "./Gamelist.css"
-class Gamelist extends Component{
-    constructor(){
-        super()
-        this.state={
-            games : []
-        }
-        this.handleRemove=this.handleRemove.bind(this)
-    }
+import React, { Component } from "react";
+import Game from "./Game";
+import "./Gamelist.css";
+class Gamelist extends Component {
+  constructor() {
+    super();
+    this.state = {
+      bestOnly: false,
+      games: []
+    };
+    this.handleRemove = this.handleRemove.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    handleRemove(id){
-        console.log(id)
+  handleRemove(index) {
+    console.log(index);
+    let newGame = [...this.state.games];
+    newGame.splice(index, 1);
+    console.log(newGame);
+    this.setState({ games: newGame });
+  }
 
+  handleSubmit() {
+    if (this.state.bestOnly === false) {
+      this.setState({ bestOnly: true });
+    } else {
+      this.setState({ bestOnly: false });
     }
-    componentDidMount(){
-        fetch("https://wild-games.herokuapp.com/api/v1")
-        .then(result => result.json())
-        .then(data => this.setState({games: data}))
-    }
-    render(){
-        return(
-        <div >
-<h1>Gamelist</h1>
-            <div className="list" >
-                
-                {this.state.games.map((game)=><Game toRemove={this.handleRemove} gameData = {game}/>)}
-            </div>
-            </div>
+  }
+  componentDidMount() {
+    fetch("https://wild-games.herokuapp.com/api/v1")
+      .then(result => result.json())
+      .then(data => this.setState({ games: data }));
+  }
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Gamelist</h1>
+          <button onClick={this.handleSubmit}>Best Games</button>
+        </header>
 
-        )
-    }
-    
+        <div className="list">
+          {this.state.games
+          .filter(game=>
+            this.state.bestOnly ? game.rating>4.5 : game.rating >0)
+            .map((game, index) => (
+                <Game
+                  number={index}
+                  toRemove={this.handleRemove}
+                  gameData={game}
+                />
+              ))
+          }
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Gamelist
+export default Gamelist;
+/* 
+
+this.state.games */
