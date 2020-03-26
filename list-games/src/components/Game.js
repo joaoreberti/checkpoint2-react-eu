845b1ded8f8f22cbe928f173baa3be42700b5158
rebertionfire <joaoreberti@gmail.com>
2020-03-26 19:22:32 +0000

    structure tree and components added

---
+++ list-games/src/components/Game.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react"
+
+class Game extends Component{
+    constructor(){
+        super()
+        this.state = {
+
+        }
+    }
+    render(){
+        return(
+            <div>
+                <h2>Game</h2>
+            </div>
+        )
+    }
+}
+
+export default Game 
---
+++ list-games/src/components/Gamelist.js
@@ -0,0 +1,21 @@
+import React, {Component} from "react"
+import Game from "./Game"
+class Gamelist extends Component{
+    constructor(){
+        super()
+        this.state={
+
+        }
+    }
+    render(){
+        return(
+            <div>
+                <h>Gamelist</h>
+            </div>
+
+        )
+    }
+    
+}
+
+export default Gamelist
