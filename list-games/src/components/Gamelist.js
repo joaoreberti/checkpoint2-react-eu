ae43e2fc1b21ef0cb83a04b8f6001f7f42e5d2e7
rebertionfire <joaoreberti@gmail.com>
2020-03-26 19:29:05 +0000

    fetch to API added to Gamelist component

--- list-games/src/components/Gamelist.js
+++ list-games/src/components/Gamelist.js
@@ -4,13 +4,21 @@ class Gamelist extends Component{
     constructor(){
         super()
         this.state={
-
+            games : []
         }
+    }
+
+
+    componentDidMount(){
+        fetch("https://wild-games.herokuapp.com/api/v1")
+        .then(result => result.json())
+        .then(data => this.setState({games: data}))
     }
     render(){
         return(
             <div>
-                <h>Gamelist</h>
+                <h1>Gamelist</h1>
+                {this.state.games.map((game)=><Game gameData = {game}/>)}
             </div>
 
         )
